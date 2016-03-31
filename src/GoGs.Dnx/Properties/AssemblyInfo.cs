567a3c1693999112f1d88e2c76309c5bdf33a3ec
David Purkiss <d_purkiss@hotmail.com>
2016-03-31 21:53:14 +0200

    fix(all): fix names
    Changed GoGs to Gogs.

--- src/GoGs.Dnx/Properties/AssemblyInfo.cs
+++ src/GoGs.Dnx/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GoGs.Dnx")]
+[assembly: AssemblyTitle("Gogs.Dnx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GoGs.Dnx")]
+[assembly: AssemblyProduct("Gogs.Dnx")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
