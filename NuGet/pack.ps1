0f67fb214ded6d2c8af26886632166388f5a8d36
David Purkiss <d_purkiss@hotmail.com>
2016-04-01 14:03:13 +0200

    chore(all): add build files for NuGet

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\Gogs.Dnx\bin\Release\Gogs.Dnx.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\src\Gogs.Dnx\Gogs.Dnx.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\src\Gogs.Dnx\Gogs.Dnx.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\src\Gogs.Dnx\Gogs.Dnx.compiled.nuspec
